e57c1d8b848ef9d4a6d83f090e1599941942dc3b
Carter Bastian <carter.bastian1@gmail.com>
2024-12-02 13:48:27 -0800

    Day 2

--- problems/day_2.py
+++ problems/day_2.py
@@ -1,61 +1,96 @@
-from lib.helpers import log, get_all_strings_by_lines
+from lib.helpers import get_all_strings_by_lines, log
+
 
 def part_1():
-    config = {
-        'red': 12,
-        'green': 13,
-        'blue': 14,
-    }
-
-    input_arr = get_all_strings_by_lines('2.txt')
+    input_arr = get_all_strings_by_lines("2.txt")
     log(input_arr)
 
-    game = 1
-    sum = 0
-    for val in input_arr:
-        possible = True
-        sets = val.split('; ')
-        for s in sets:
-            pulls = s.split(', ')
-            for pull in pulls:
-                count_s, color = pull.split(' ')
-                count = int(count_s)
-                # log(f'Game {game}: {s}: {pull}: {count} {color}')
-                if count > config[color]:
-                    log(f'Game {game}: {color} is over the limit!')
-                    possible = False
+    count = 0
+    for line in input_arr:
+        numbers = [int(x) for x in line.split(" ")]
+        if safe_checker(numbers, safety_run=True):
+            count += 1
 
-        if possible:
-            log(f'Game {game} is possible!')
-            sum += game
+    log(count)
 
-        game += 1
+    return count
 
-    return sum
+
+def safe_checker(numbers, safety_run=False):
+    cur_num = numbers[0]
+    dir = None
+    for idx, number in enumerate(numbers):
+        if idx == 0:
+            continue
+        if dir is None:
+            if number > cur_num:
+                dir = "right"
+            elif number < cur_num:
+                dir = "left"
+            else:
+                if safety_run:
+                    return False
+                else:
+                    return any(
+                        [
+                            safe_checker(numbers[1:], safety_run=True),
+                            safe_checker([numbers[0]] + numbers[2:], safety_run=True),
+                        ]
+                    )
+
+        if dir == "right":
+            if number not in [cur_num + 1, cur_num + 2, cur_num + 3]:
+                if safety_run:
+                    return False
+                else:
+                    if idx == len(numbers) - 1:
+                        return True
+                    return any(
+                        [
+                            safe_checker(
+                                numbers[0:idx] + numbers[idx + 1 :], safety_run=True
+                            ),
+                            safe_checker(
+                                numbers[0 : idx + 1] + numbers[idx + 2 :],
+                                safety_run=True,
+                            ),
+                        ]
+                    )
+        elif dir == "left":
+            if number not in [cur_num - 1, cur_num - 2, cur_num - 3]:
+                if safety_run:
+                    return False
+                else:
+                    if idx == len(numbers) - 1:
+                        return True
+                    return any(
+                        [
+                            safe_checker(
+                                numbers[0:idx] + numbers[idx + 1 :], safety_run=True
+                            ),
+                            safe_checker(
+                                numbers[0 : idx + 1] + numbers[idx + 2 :],
+                                safety_run=True,
+                            ),
+                        ]
+                    )
+        cur_num = number
+
+    print(f"safe_checker {numbers} returned True")
+    return True
 
 
 def part_2():
-    input_arr = get_all_strings_by_lines('2.txt')
+    input_arr = get_all_strings_by_lines("2.txt")
+    log(input_arr)
+    count = 0
+    for line in input_arr:
+        numbers = [int(x) for x in line.split(" ")]
+        if safe_checker(numbers, safety_run=False) or safe_checker(
+            numbers[::-1], safety_run=False
+        ):
+            count += 1
 
-    sum = 0
-    for val in input_arr:
-        sets = val.split('; ')
-        min_config = {
-            'red': 0,
-            'green': 0,
-            'blue': 0,
-        }
-        for s in sets:
-            pulls = s.split(', ')
-            for pull in pulls:
-                count_s, color = pull.split(' ')
-                count = int(count_s)
-                # log(f'Game {game}: {s}: {pull}: {count} {color}')
-                if count > min_config[color]:
-                    min_config[color] = count
+    log(count)
 
-        # Find the power and add it to the sum
-        sum += min_config['red'] * min_config['green'] * min_config['blue']
-
-
-    return sum
+    return count
