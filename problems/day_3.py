1a2725299a002b925831062f335b4ec8e5204406
Carter Bastian <carter.bastian1@gmail.com>
2024-12-02 21:14:16 -0800

    Day 3

--- problems/day_3.py
+++ problems/day_3.py
@@ -1,19 +1,41 @@
-from lib.helpers import get_all_strings_by_lines, log
+import re
+
+from lib.helpers import get_all_strings_by_lines, get_input, log
 
 
 def part_1():
     input_arr = get_all_strings_by_lines("3.txt")
-    log(input_arr)
+    log(len(input_arr))
 
     sum = 0
+    for line in input_arr:
+        # Write an re that matches `mul(a,b)` and extract a and b
+        matches = re.finditer(r"mul\(([0-9]+),([0-9]+)\)", line)
+        for match in matches:
+            a = int(match.group(1))
+            b = int(match.group(2))
+            sum += a * b
 
     return sum
 
 
 def part_2():
-    input_arr = get_all_strings_by_lines("3.txt")
-    log(input_arr)
+    input_str = get_input("3.txt")
+    sections = input_str.split("don't()")
+    re_created = sections[0]
+    for section in sections[1:]:
+        # Ignore everything up to the first `do()`
+        salvage = section.split("do()")[1:]
+        re_created += "".join(salvage)
+
+    log(re_created)
 
     sum = 0
+    # Write an re that matches `mul(a,b)` and extract a and b
+    matches = re.finditer(r"mul\(([0-9]+),([0-9]+)\)", re_created)
+    for match in matches:
+        a = int(match.group(1))
+        b = int(match.group(2))
+        sum += a * b
 
     return sum
