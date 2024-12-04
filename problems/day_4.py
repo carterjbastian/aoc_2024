95e2af6cf229c4a2f7ea0dd9be4740e6089cc661
Carter Bastian <carter.bastian1@gmail.com>
2024-12-03 21:27:34 -0800

    Day 4

--- problems/day_4.py
+++ problems/day_4.py
@@ -1,15 +1,185 @@
-from lib.helpers import get_all_strings_by_lines
+from lib.helpers import get_strings_by_lines
+
+
+def check_right(arr, i, j):
+    if j + 4 > len(arr[i]):
+        return False
+
+    if arr[i][j + 1] == "M" and arr[i][j + 2] == "A" and arr[i][j + 3] == "S":
+        return True
+
+    return False
+
+
+def check_left(arr, i, j):
+    if j < 3:  # Need at least 3 positions to the left
+        return False
+
+    if arr[i][j - 1] == "M" and arr[i][j - 2] == "A" and arr[i][j - 3] == "S":
+        return True
+
+    return False
+
+
+def check_up(arr, i, j):
+    if i < 3:  # Need at least 3 positions above
+        return False
+
+    if arr[i - 1][j] == "M" and arr[i - 2][j] == "A" and arr[i - 3][j] == "S":
+        return True
+
+    return False
+
+
+def check_down(arr, i, j):
+    if i + 4 > len(arr):  # Need at least 3 positions below
+        return False
+
+    if arr[i + 1][j] == "M" and arr[i + 2][j] == "A" and arr[i + 3][j] == "S":
+        return True
+
+    return False
+
+
+def check_up_right(arr, i, j):
+    if i < 3 or j + 4 > len(arr[i]):  # Need 3 positions above and 3 to the right
+        return False
+
+    if (
+        arr[i - 1][j + 1] == "M"
+        and arr[i - 2][j + 2] == "A"
+        and arr[i - 3][j + 3] == "S"
+    ):
+        return True
+
+    return False
+
+
+def check_up_left(arr, i, j):
+    if i < 3 or j < 3:  # Need 3 positions above and 3 to the left
+        return False
+
+    if (
+        arr[i - 1][j - 1] == "M"
+        and arr[i - 2][j - 2] == "A"
+        and arr[i - 3][j - 3] == "S"
+    ):
+        return True
+
+    return False
+
+
+def check_down_right(arr, i, j):
+    if i + 4 > len(arr) or j + 4 > len(
+        arr[i]
+    ):  # Need 3 positions below and 3 to the right
+        return False
+
+    if (
+        arr[i + 1][j + 1] == "M"
+        and arr[i + 2][j + 2] == "A"
+        and arr[i + 3][j + 3] == "S"
+    ):
+        return True
+
+    return False
+
+
+def check_down_left(arr, i, j):
+    if i + 4 > len(arr) or j < 3:  # Need 3 positions below and 3 to the left
+        return False
+
+    if (
+        arr[i + 1][j - 1] == "M"
+        and arr[i + 2][j - 2] == "A"
+        and arr[i + 3][j - 3] == "S"
+    ):
+        return True
+
+    return False
+
+
+def find_matches(arr):
+    total = 0
+    for i, row in enumerate(arr):
+        for j, col in enumerate(row):
+            if col == "X":
+                if check_right(arr, i, j):
+                    total += 1
+                if check_left(arr, i, j):
+                    total += 1
+                if check_up(arr, i, j):
+                    total += 1
+                if check_down(arr, i, j):
+                    total += 1
+                if check_up_right(arr, i, j):
+                    total += 1
+                if check_up_left(arr, i, j):
+                    total += 1
+                if check_down_right(arr, i, j):
+                    total += 1
+                if check_down_left(arr, i, j):
+                    total += 1
+
+    return total
 
 
 def part_1():
-    input_arr = get_all_strings_by_lines("4.txt")
-    sum = 0
+    input_arr = get_strings_by_lines("4.txt")
+    arr = [[c for c in l] for l in input_arr]
 
-    return sum
+    return find_matches(arr)
+
+
+def find_mas_right(arr, i, j):
+    if i + 3 > len(arr) or j + 3 > len(
+        arr[i]
+    ):  # Need 3 positions below and 3 to the right
+        return False
+
+    if (
+        arr[i + 1][j + 1] == "A"
+        and arr[i + 2][j + 2] == "S"
+        and (
+            (arr[i][j + 2] == "M" and arr[i + 2][j] == "S")
+            or (arr[i][j + 2] == "S" and arr[i + 2][j] == "M")
+        )
+    ):
+        return True
+
+
+def find_sam_right(arr, i, j):
+    if i + 3 > len(arr) or j + 3 > len(
+        arr[i]
+    ):  # Need 3 positions below and 3 to the right
+        return False
+
+    if (
+        arr[i + 1][j + 1] == "A"
+        and arr[i + 2][j + 2] == "M"
+        and (
+            (arr[i][j + 2] == "M" and arr[i + 2][j] == "S")
+            or (arr[i][j + 2] == "S" and arr[i + 2][j] == "M")
+        )
+    ):
+        return True
 
 
 def part_2():
-    input_arr = get_all_strings_by_lines("4.txt")
+    input_arr = get_strings_by_lines("4.txt")
     sum = 0
+    arr = [[c for c in l] for l in input_arr]
+    new_arr = [["." for _ in l] for l in input_arr]
+
+    for i, row in enumerate(arr):
+        for j, col in enumerate(row):
+            if col == "M":
+                if find_mas_right(arr, i, j):
+                    new_arr[i][j] = "M"
+                    sum += 1
+            elif col == "S":
+                if find_sam_right(arr, i, j):
+                    new_arr[i][j] = "S"
+                    sum += 1
 
     return sum
--- problems/day_5.py
+++ problems/day_5.py
@@ -1,86 +1,16 @@
-from lib.helpers import log, get_input
+from lib.helpers import get_input
 
-def convert(val, conversion_rules):
-    for rule in conversion_rules:
-        start_dest, start_source, range_len = rule
-        if start_source <= val < start_source + range_len:
-            return start_dest + (val - start_source)
-    
-    return val
-
-def invert(val, conversion_rules):
-    for rule in conversion_rules:
-        start_dest, start_source, range_len = rule
-        if start_dest <= val < start_dest + range_len:
-            return start_source + (val - start_dest)
-    
-    return val
 
 def part_1():
-    input_arr = get_input('5.txt')
-    substrings = input_arr.split('\n\n')
-    seed_s = substrings[0]
-    conversion_strings = substrings[1:]
-    conversions = []
-    seed = [int(val) for val in seed_s.split(' ') if val != '']
-    log(seed)
-    log(conversion_strings[-1])
+    input_arr = get_input("5.txt")
+    substrings = input_arr.split("\n\n")
 
-    for conv_s in conversion_strings:
-        rules = []
-        for rule_s in conv_s.split('\n'):
-            rules.append(tuple([int(val) for val in rule_s.split(' ') if val != '']))
-        
-        conversions.append(rules)
-
-    log(conversions)
-
-    min = float('inf')
-    for seed_val in seed:
-        log('\n\nSeed: ' + str(seed_val))
-        cur = seed_val
-        for conversion in conversions:
-            cur = convert(cur, conversion)
-            log(cur)
-        
-        if (cur < min):
-            min = cur
-
+    min = 0
     return min
 
+
 def part_2():
-    input_arr = get_input('5.txt')
-    substrings = input_arr.split('\n\n')
-    seed_s = substrings[0]
-    conversion_strings = substrings[1:]
-    conversions = []
-    seeds = [int(val) for val in seed_s.split(' ') if val != '']
-
-    for conv_s in conversion_strings:
-        rules = []
-        for rule_s in conv_s.split('\n'):
-            rules.append(tuple([int(val) for val in rule_s.split(' ') if val != '']))
-        
-        conversions.append(rules)
-
-    conversions.reverse()
-    location = 0
-    while True:
-        # Upper bound found with some manual binary searching
-        if location == 80000000:
-            return 0
-        if location % 1000000 == 0:
-            log(f'Checking Location: {location}')
-
-        cur = location
-        for conversion in conversions:
-            cur = invert(cur, conversion)
-        
-        for idx in range(int(len(seeds) / 2)):
-            # Work backwards from the location
-            start = seeds[idx * 2]
-            range_len = seeds[(idx * 2) + 1]
-            if start <= cur < start + range_len:
-                return location
-        # Change this to 100 to scan for the range
-        location += 1
+    input_arr = get_input("5.txt")
+    substrings = input_arr.split("\n\n")
+    min = 0
+    return min
