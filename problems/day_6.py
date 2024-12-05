f537c2c3263320dc185a6d05f0ad9387eb3a5aac
Carter Bastian <carter.bastian1@gmail.com>
2024-12-04 21:38:59 -0800

    Day 5

--- problems/day_6.py
+++ problems/day_6.py
@@ -1,51 +1,13 @@
-from lib.helpers import log, get_strings_by_lines, TEST_MODE
+from lib.helpers import get_strings_by_lines
 
-
-# D(wait) = (t - wait) * wait = wait * t - wait ^ 2
-distance = lambda t, wait: wait * t - (wait ** 2)
-
-def count_solutions(time, min_dist):
-    count = 0
-    for wait in range(1, time):
-        if distance(time, wait) > min_dist:
-            count += 1
-    
-    return count
-
-def first_solution(time, min_dist):
-    for wait in range(1, time):
-        if distance(time, wait) > min_dist:
-            return wait
-
-def last_solution(time, min_dist):
-    for wait in range(time, 1, -1):
-        if distance(time, wait) > min_dist:
-            return wait
-    
 
 def part_1():
-    input_arr = get_strings_by_lines('6.txt')
-    times = [int(t) for t in input_arr[0].split(' ') if t != '']
-    dist = [int(d) for d in input_arr[1].split(' ') if d != '']
+    input_arr = get_strings_by_lines("6.txt")
 
-    log(times)
-    log(dist)
+    return len(input_arr)
 
-    product = 1
-    for i in range(len(times)):
-        log(f"Time: {times[i]}, Distance: {dist[i]}")
-        solutions = count_solutions(times[i], dist[i])
-        log(f"Found: {solutions} solutions\n")
-        product *= solutions
-
-    return product
 
 def part_2():
-    times = [71530] if TEST_MODE else [49787980]
-    distance = [940200] if TEST_MODE else [298118510661181]
+    input_arr = get_strings_by_lines("6.txt")
 
-    first = first_solution(times[0], distance[0])
-    log(f"First solution: {first}")
-    last = last_solution(times[0], distance[0])
-    log(f"Last solution: {last}")
-    return last - first + 1
+    return len(input_arr)
