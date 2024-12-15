aa24e7659a889984ecbd1d16a0048fbb5d9dfbd3
Carter Bastian <carter.bastian1@gmail.com>
2024-12-14 16:02:49 -0800

    Finally finish Day 13

--- problems/day_13.py
+++ problems/day_13.py
@@ -1,57 +1,60 @@
 from lib.helpers import get_input, log
 
 
+def kramers_rule(prize_x, prize_y, a_x, a_y, b_x, b_y, offset):
+    target_x, target_y = prize_x + offset, prize_y + offset
+    determinant = (a_x * b_y) - (a_y * b_x)
+    # Solve for A and B rounded to the nearest integer
+    A = round(((target_x * b_y) - (target_y * b_x)) / determinant)
+    B = round(((a_x * target_y) - (a_y * target_x)) / determinant)
+    log(f"target_x: {target_x}, target_y: {target_y}")
+    if a_x * A + b_x * B != target_x:
+        log(f"No Solution: a_x * A + b_x * B != target_x, A: {A}, B: {B}")
+        return -1, -1
+    if a_y * A + b_y * B != target_y:
+        log(f"No Solution: a_y * A + b_y * B != target_y, A: {A}, B: {B}")
+        return -1, -1
+    return A, B
+
+
 def part_1():
     txt = get_input("13.txt")
     machine_strs = txt.split("\n\n")
+    offset = 0
 
     total_cost = 0
     for i, machine_str in enumerate(machine_strs):
-        log(f"Machine {i}")
+        log(f"\nMachine {i}\n")
         mach_a, mach_b, prize = machine_str.split("\n")
         mach_a = mach_a[12:]
         mach_a_x, mach_a_y = mach_a.split(", Y+")
+        mach_a_x = int(mach_a_x)
+        mach_a_y = int(mach_a_y)
         log(f"mach_a_x: {mach_a_x}, mach_a_y: {mach_a_y}")
         mach_b = mach_b[12:]
         mach_b_x, mach_b_y = mach_b.split(", Y+")
-        log(f"mach_b_x: {mach_b_x}, mach_b_y: {mach_b_y}")
+        mach_b_x = int(mach_b_x)
+        mach_b_y = int(mach_b_y)
+        log(f"mach_b_x: {mach_b_x}, mach_b_y: {mach_b_y}\n")
         prize = prize[9:]
         prize_x, prize_y = prize.split(", Y=")
-        log(f"prize_x: {prize_x}, prize_y: {prize_y}")
-        log(machine_str)
+        prize_x = int(prize_x)
+        prize_y = int(prize_y)
 
-        # Press the B button (for 1 token) as many times as possible
-        b_count = 0
-        possible_b_counts = []
-        best_config = None
-        best_cost = None
-        while b_count < 100:
-            x_cur = int(mach_b_x) * b_count
-            y_cur = int(mach_b_y) * b_count
-            if (
-                (int(prize_x) - x_cur) % int(mach_a_x) == 0
-                and (int(prize_y) - y_cur) % int(mach_a_y) == 0
-                and (
-                    (int(prize_x) - x_cur) // int(mach_a_x)
-                    == (int(prize_y) - y_cur) // int(mach_a_y)
-                )
-            ):
-                a_count = (int(prize_x) - x_cur) // int(mach_a_x)
-                log(f"Possible config = {b_count} b-presses + {a_count} a-presses")
-                if a_count > 100:
-                    log("\ta_count > 100, skipping")
-                    continue
-                possible_b_counts.append(b_count)
-                cost = b_count + (3 * a_count)
-                if best_cost is None or cost < best_cost:
-                    log("\tcost < best_cost, updating best_cost and best_config")
-                    best_cost = cost
-                    best_config = (b_count, a_count)
+        # Start here:
+        A, B = kramers_rule(
+            prize_x, prize_y, mach_a_x, mach_a_y, mach_b_x, mach_b_y, offset
+        )
+        log(f"A: {A}, B: {B}")
+        if A < 0:
+            log(f"No Solution: A < 0, A: {A}, B: {B}")
+            continue
+        if B < 0:
+            log(f"No Solution: B < 0, A: {A}, B: {B}")
+            continue
 
-            b_count += 1
-        log(f"best_cost: {best_cost}")
-        log(f"best_config: {best_config}")
-        total_cost += best_cost if best_config is not None else 0
+        log(f"Found solution: A: {A}, B: {B}")
+        total_cost += (A * 3) + B
 
     return total_cost
 
@@ -66,14 +69,13 @@ def get_factors(n):
 
 
 def part_2():
-    txt = get_input("13.txt")
-
+    offset = 10000000000000
     txt = get_input("13.txt")
     machine_strs = txt.split("\n\n")
 
     total_cost = 0
     for i, machine_str in enumerate(machine_strs):
-        log(f"Machine {i}")
+        log(f"\nMachine {i}\n")
         mach_a, mach_b, prize = machine_str.split("\n")
         mach_a = mach_a[12:]
         mach_a_x, mach_a_y = mach_a.split(", Y+")
@@ -84,52 +86,25 @@ def part_2():
         mach_b_x, mach_b_y = mach_b.split(", Y+")
         mach_b_x = int(mach_b_x)
         mach_b_y = int(mach_b_y)
-        log(f"mach_b_x: {mach_b_x}, mach_b_y: {mach_b_y}")
+        log(f"mach_b_x: {mach_b_x}, mach_b_y: {mach_b_y}\n")
         prize = prize[9:]
         prize_x, prize_y = prize.split(", Y=")
-        prize_x = int(prize_x) + 10000000000000
-        prize_y = int(prize_y) + 10000000000000
-        prize_x_factors = get_factors(prize_x)
-        prize_y_factors = get_factors(prize_y)
-        log(f"prize_x_factors: {prize_x_factors}")
-        log(f"prize_y_factors: {prize_y_factors}")
+        prize_x = int(prize_x)
+        prize_y = int(prize_y)
 
-        # Find the factors of prize_x and prize_y
+        # Start here:
+        A, B = kramers_rule(
+            prize_x, prize_y, mach_a_x, mach_a_y, mach_b_x, mach_b_y, offset
+        )
+        log(f"A: {A}, B: {B}")
+        if A < 0:
+            log(f"No Solution: A < 0, A: {A}, B: {B}")
+            continue
+        if B < 0:
+            log(f"No Solution: B < 0, A: {A}, B: {B}")
+            continue
 
-        log(f"prize_x: {prize_x}, prize_y: {prize_y}")
-        log(machine_str)
-
-        # Press the B button (for 1 token) as many times as possible
-        possible_b_counts = []
-        best_config = None
-        best_cost = None
-        b_upper_bound = min(prize_x // mach_b_x, prize_y // mach_b_y)
-        b_count = b_upper_bound
-        print(f"b_upper_bound: {b_upper_bound}")
-        while b_count >= 0:
-            x_cur = mach_b_x * b_count
-            y_cur = mach_b_y * b_count
-            if (
-                (prize_x - x_cur) % mach_a_x == 0
-                and (prize_y - y_cur) % mach_a_y == 0
-                and ((prize_x - x_cur) // mach_a_x == (prize_y - y_cur) // mach_a_y)
-            ):
-                a_count = (prize_x - x_cur) // mach_a_x
-                log(f"Possible config = {b_count} b-presses + {a_count} a-presses")
-                if a_count > 100:
-                    log("\ta_count > 100, skipping")
-                    continue
-                possible_b_counts.append(b_count)
-                cost = b_count + (3 * a_count)
-                if best_cost is None or cost < best_cost:
-                    log("\tcost < best_cost, updating best_cost and best_config")
-                    best_cost = cost
-                    best_config = (b_count, a_count)
-                    break
-
-            b_count -= 1
-        log(f"best_cost: {best_cost}")
-        log(f"best_config: {best_config}")
-        total_cost += best_cost if best_config is not None else 0
+        log(f"Found solution: A: {A}, B: {B}")
+        total_cost += (A * 3) + B
 
     return total_cost
---
+++ problems/day_15.py
@@ -0,0 +1,15 @@
+from lib.helpers import get_all_strings_by_lines, log
+
+
+def part_1():
+    input_arr = get_all_strings_by_lines("15.txt")
+
+    log(input_arr)
+    return len(input_arr)
+
+
+def part_2():
+    input_arr = get_all_strings_by_lines("15.txt")
+
+    log(input_arr)
+    return len(input_arr)
